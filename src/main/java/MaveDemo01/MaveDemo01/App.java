f7f66fa24a3ec1573a812ee8dc68ee52d85cb869
tangjiang@bluemoon.com.cn <tangjiang@bluemoon.com.cn>
2018-04-04 14:20:37 +0800

    This is test

---
+++ src/main/java/MaveDemo01/MaveDemo01/App.java
@@ -0,0 +1,13 @@
+package MaveDemo01.MaveDemo01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MaveDemo01/MaveDemo01/TestDemo.java
@@ -0,0 +1,11 @@
+package MaveDemo01.MaveDemo01;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	@Test
+	public void test() {
+		System.out.println("this is test");
+	}
+
+}
